f6e4ff5896fd39ea5d62cec1826b5a4f592c623b
Raymond Dzery Hago <60923484+RDH36@users.noreply.github.com>
2024-10-07 18:44:47 +0300

    feat: page dialogue integration (#3)

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import { FormToGenerate } from "@/components/form-generate/FormToGenerate";
 import HeroBanner from "@/components/hero/HeroBanner";
 
 export default function Home() {
   return (
-    <div className="h-full w-full">
+    <div className="h-full w-full flex flex-col items-center mt-10 gap-10">
       <HeroBanner />
+      <FormToGenerate />
     </div>
   );
 }
