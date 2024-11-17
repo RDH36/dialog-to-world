f0a32cff697db3f22989a48a3637d0e48b8fd805
rdh <simplice2236@gmail.com>
2024-11-17 13:43:40 +0300

    feat: update ui landing page

--- components/feature/hero/HeroBanner.tsx
+++ components/feature/hero/HeroBanner.tsx
@@ -1,3 +1,6 @@
+import GetStarted from "@/components/auth/GetStarted";
+import { CompassIcon } from "lucide-react";
+import Link from "next/link";
 import TypewriterEffect from "../TypewriterEffect/TypewriterEffect";
 
 export default function HeroBanner() {
@@ -8,6 +11,17 @@ export default function HeroBanner() {
         Discover a new language through realistic and captivating dialogues with
         AI.
       </p>
+
+      <div className="flex flex-col sm:flex-row gap-4 pt-4 justify-center">
+        <GetStarted />
+        <Link
+          href="/explore"
+          className="px-8 py-3 text-base font-medium  bg-gray-400/20 border  rounded-lg hover:bg-gray-200 dark:hover:bg-gray-700 transition-colors inline-flex items-center justify-center"
+        >
+          <CompassIcon className="w-5 h-5 mr-2" />
+          Explore
+        </Link>
+      </div>
     </div>
   );
 }
