8ba0815f2148138a07aa2371641236c8ebe9a0b1
Raymond Dzery Hago <60923484+RDH36@users.noreply.github.com>
2024-11-26 19:13:27 +0300

    feat: add login page (#33)

--- app/dashboard/create/page.tsx
+++ app/dashboard/create/page.tsx
@@ -8,7 +8,7 @@ export default function Page() {
       <div className="max-w-2xl mx-auto">
         <div className="text-center mb-8">
           <h1 className="text-3xl font-bold text-gray-900 dark:text-gray-100">
-            Lets Create amaizing Dialogue
+            Lets Create amazing Dialogue
           </h1>
           <p className="mt-2 text-sm text-gray-600 dark:text-gray-400">
             simple and easy to use, enjoy the process
--- components/auth/AuthButton.tsx
+++ components/auth/AuthButton.tsx
@@ -1,24 +1,10 @@
-"use client";
 import { Button } from "@/components/ui/button";
-import { Loader2 } from "lucide-react";
-import { useState } from "react";
-import { signInAuth } from "./auth.action";
+import Link from "next/link";
 
 export default function AuthButton() {
-  const [isPending, setIsPending] = useState(false);
   return (
-    <div>
-      <Button
-        className="bg-primary text-white rounded-sm font-medium"
-        onClick={async () => {
-          setIsPending(true);
-          await signInAuth();
-          setIsPending(false);
-        }}
-      >
-        {isPending ? <Loader2 className="animate-spin mr-2" size={16} /> : null}
-        Login
-      </Button>
-    </div>
+    <Button className="bg-primary text-white rounded-sm font-medium">
+      <Link href="/login"> Sign in</Link>
+    </Button>
   );
 }
