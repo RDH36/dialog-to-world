d034d3ff46db8fc77bb2458dd805263fa58fcbff
rdh <simplice2236@gmail.com>
2024-11-26 19:23:37 +0300

    fix: redirect to create

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,5 +1,11 @@
+import { auth } from "@/auth";
 import { LoginPage } from "@/components/auth/login-page";
+import { redirect } from "next/navigation";
 
-export default function Login() {
+export default async function Login() {
+  const session = await auth();
+  if (session?.user) {
+    redirect("/dashboard/create");
+  }
   return <LoginPage />;
 }
